8f8f1965465857555097a1c3b1427f7fd5eb84f3
Name-3R1C <ericfeng94@hotmail.com>
2023-09-11 18:24:52 -0700

    implement score fn and start fn

--- Question.rb
+++ Question.rb
@@ -1,14 +1,16 @@
 class Question
+  attr_reader :num1, :num2
+
   def initialize
-    @num1 = rand(1..10)
-    @num2 = rand(1..10)
+    @num1 = rand(1..20)
+    @num2 = rand(1..20)
   end
 
   def question
-    puts "What does #{@num1} plus #{@num2} equal?"
+    puts "What does #{@num1} plus #{@num2} equal ?"
   end
 
   def correct?(answer)
-    
+    return answer == (@num1 + @num2)
   end
 end
--- main.rb
+++ main.rb
@@ -1,16 +1,34 @@
-require "./Game"
 require "./Player"
 require "./Question"
 
+class Game
+  def initialize
+    @players = [Player.new("Player 1"), Player.new("Player 2")]
+  end
+
+  def start
+    puts @players[0].name
+    puts @players[1].name
+    current = 0
+    while @players[0].live > 0 && @players[1].live > 0
+      question = Question.new()
+      question.question
+      self.score
+      @players[0].lose_life
+    end
+  end
+
+  def score
+    puts "P1: #{@players[0].live}/3 vs P2: #{@players[1].live}/3"
+  end
+end
 # puts "tesing "
 # answer = gets.chomp
 # puts answer
 
-# player1 = Player.new("Player 1")
-# puts player1.name
-# puts player1.live
-# player1.lose_life
-# puts player1.live
+# puts question.num1
+# puts question.num2
+# puts question.correct?(5)
 
-question = Question.new()
-puts question.num1
+game = Game.new()
+game.start()
