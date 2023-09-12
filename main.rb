7550418647d90f33a6fd1fc4c547091221f2cabd
Name-3R1C <ericfeng94@hotmail.com>
2023-09-11 18:58:51 -0700

    fix msg order

--- main.rb
+++ main.rb
@@ -9,26 +9,27 @@ class Game
   end
 
   def start
-    current = 0
+    currentPlayer = 0
     loop do
       question = Question.new()
-      question.question(players[current].name)
+      question.question(players[currentPlayer].name)
       
       answer = gets.chomp.to_i
       if question.correct?(answer)
         puts "YES! You are correct."
       else
         puts "Seriously? No!"
-        players[current].lose_life
+        players[currentPlayer].lose_life
       end
 
+      # break loop if either has 0 live
       if players[0].live <= 0 || players[1].live <= 0
         break
       end
-
-      puts "----- NEW TURN -----"
+      
       self.score
-      current = (current + 1) % 2
+      puts "----- NEW TURN -----"
+      currentPlayer = (currentPlayer + 1) % 2
     end
     
     self.winner(players[0].live > 0 ? 0 : 1)
